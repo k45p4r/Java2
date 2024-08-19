428f83b8ef79b527542aee9cb6de12cdfe741f5a
k45p4r <kaspar.vainonen@metropolia.fi>
2024-08-19 10:57:32 +0300

    push

--- src/mod1_1/Task3.java
+++ src/mod1_1/Task3.java
@@ -17,5 +17,7 @@ public class SumOfThreeNumbers {
         int third = Integer.parseInt(scanner.nextLine());
 
         System.out.println("The sum of the numbers is " + (first + second + third));
+        System.out.println("The average of the numbers is " + (first + second + third) / 3.0);
+        System.out.println("The product of the numbers is " + (first * second * third));
     }
 }
